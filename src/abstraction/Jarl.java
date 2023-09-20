7ece0cdedd1e2c7f0c2a669440dc1fa5f57a6ab1
jarlkeenen <sarmientojarl.aws@gmail.com>
2023-09-20 15:30:57 +0800

    Created Jarl class

---
+++ src/abstraction/Jarl.java
@@ -0,0 +1,11 @@
+package abstraction;
+
+public class Jarl extends Animal {
+	void talk() {
+		System.out.println("Jarl Sarmiento is talking.");
+	}
+	
+	void type() {
+		System.out.println("Jarl Sarmiento is typing.");
+	}
+}
