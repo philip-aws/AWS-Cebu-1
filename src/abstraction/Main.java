e96a8373b1473666cec2ece5592dd3c3a4895e87
philip-aws <bencalophilip.aws@gmail.com>
2023-09-20 15:53:51 +0800

    Initializes classes in main

--- src/abstraction/Main.java
+++ src/abstraction/Main.java
@@ -3,8 +3,19 @@ package abstraction;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Animal dinosaur = new Dinosaur();
+		Animal dog = new Dog();
+		Animal doubutsu = new Doubutsu();
+		Animal fish = new Fish();
+		Animal jarl = new Jarl();
+		Animal lion = new Lion();
+		
+		dinosaur.talk();
+		dog.talk();
+		doubutsu.talk();
+		fish.talk();
+		jarl.talk();
+		lion.talk();
 	}
 
 }
