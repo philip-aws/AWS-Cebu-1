b70b0d2cd955a28b6a8e51183ccf9d7576e66b27
MaryParagoso <paragosomacarmel.aws@gmail.com>
2023-09-25 15:22:10 +0800

    Added dog class

--- src/abstraction/Dog.java
+++ src/abstraction/Dog.java
@@ -1,10 +1,39 @@
-package abstraction;
+package mapper;
 
-public class Dog extends Animal{
-	void talk() {
-		System.out.println("Arff... Arff...");
+public class Dog implements Animal{
+	@Override
+	public String name() {
+		// TODO Auto-generated method stub
+		return "Dog";
 	}
-	void type() {
-		System.out.println("Canine");
+
+	@Override
+	public String sound() {
+		// TODO Auto-generated method stub
+		return "~Arff~Arff~Arff~";
+	}
+
+	@Override
+	public String type() {
+		// TODO Auto-generated method stub
+		return "Mammal";
+	}
+
+	@Override
+	public String description() {
+		// TODO Auto-generated method stub
+		return "Dogs are domesticated mammals known for their wide range of breeds, sizes, and personalities. "
+				+ "They are loyal, social animals that have been human companions for thousands of years, serving as pets, working animals, and sources of comfort and joy in homes around the world.";
+	}
+
+	@Override
+	public int cageNumber() {
+		// TODO Auto-generated method stub
+		return 120;
+	}
+
+	@Override
+	public String toString() {
+		return "Name: " + name() + "\n" + "Type: " + type() + "\n" + "Description: " + description();
 	}
 }
